ec81eab074cf91b47e2af756236782a27d1925c9
Vivien Tóth <tvivu.59@gmail.com>
2020-12-11 22:01:30 +0100

    A Beteg osztályt felhasználva egy kicsit módosítottam

--- Korona/Korona/Beteg.cs
+++ Korona/Korona/Beteg.cs
@@ -9,10 +9,11 @@ namespace Korona
     public class Beteg
     {
         public int Id { get; set; }
-        public string Beteg_neve { get; set; }
+        public string Nev { get; set; }
+        public int Eletkor { get; set; }
         public string Elerhetoseg { get; set; }
         public string Neme { get; set; }
         public int Tajszam { get; set; }
-        public int Eletkor { get; set; }
+       
     }
 }
--- Korona/Korona/Form1.cs
+++ Korona/Korona/Form1.cs
@@ -22,11 +22,22 @@ namespace Korona
         private void save_btn_Click(object sender, EventArgs e)
         {
             connection.Open();
-            string query = "INSERT INTO Betegek (Id,Nev,Eletkor,Elerhetoseg,Neme,Taj_szam) VALUES('"+ idTextBox.Text+ "','" + nevTextBox.Text + "','" + eletkorTextBox.Text + "','" + elerhetosegTextBox.Text + "','" + nemeComboBox.Text + "','" + tajszamTextBox.Text + "')";
+            Beteg beteg = new Beteg();
+            beteg.Id = Int32.Parse(idTextBox.Text);
+            beteg.Nev = nevTextBox.Text;
+            beteg.Eletkor = Int32.Parse(eletkorTextBox.Text);
+            beteg.Elerhetoseg = elerhetosegTextBox.Text;
+            beteg.Neme = nemeComboBox.Text;
+            beteg.Tajszam = Int32.Parse(tajszamTextBox.Text);
+
+            String query = "INSERT INTO Betegek (Id,Nev,Eletkor,Elerhetoseg,Neme,Taj_szam) VALUES('"+beteg.Id + "','" +beteg.Nev+ "','" +beteg.Eletkor+ "','" +beteg.Elerhetoseg+ "','" +beteg.Neme+ "','" +beteg.Tajszam+ "')";
             
             SqlDataAdapter SDA = new SqlDataAdapter(query,connection);
+                        
             SDA.SelectCommand.ExecuteNonQuery();
+            
             connection.Close();
+            
             MessageBox.Show("Sikeresen elmentve");
         }
 
