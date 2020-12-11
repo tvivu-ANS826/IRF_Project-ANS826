3dcd8863e00b70042d8ad91ff49d968a7d895747
Vivien Tóth <tvivu.59@gmail.com>
2020-12-11 21:29:39 +0100

    Már a program tudja, ha beviszünk adatot, akkor az adatbázisba bekerül

--- Korona/Korona/Form1.Designer.cs
+++ Korona/Korona/Form1.Designer.cs
@@ -29,74 +29,50 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.comboBox4 = new System.Windows.Forms.ComboBox();
-            this.comboBox5 = new System.Windows.Forms.ComboBox();
-            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.nemeComboBox = new System.Windows.Forms.ComboBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.save_btn = new System.Windows.Forms.Button();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.nevTextBox = new System.Windows.Forms.TextBox();
+            this.elerhetosegTextBox = new System.Windows.Forms.TextBox();
+            this.tajszamTextBox = new System.Windows.Forms.TextBox();
+            this.eletkorTextBox = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.groupBox1.Controls.Add(this.eletkorTextBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.elerhetosegTextBox);
             this.groupBox1.Controls.Add(this.save_btn);
+            this.groupBox1.Controls.Add(this.tajszamTextBox);
+            this.groupBox1.Controls.Add(this.nevTextBox);
+            this.groupBox1.Controls.Add(this.idTextBox);
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.comboBox6);
-            this.groupBox1.Controls.Add(this.comboBox5);
-            this.groupBox1.Controls.Add(this.comboBox4);
+            this.groupBox1.Controls.Add(this.nemeComboBox);
             this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.comboBox3);
-            this.groupBox1.Controls.Add(this.comboBox2);
-            this.groupBox1.Controls.Add(this.comboBox1);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(28, 23);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(496, 387);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Beteg felvétele";
             // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(180, 36);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 28);
-            this.comboBox1.TabIndex = 0;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(180, 85);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 28);
-            this.comboBox2.TabIndex = 1;
-            // 
-            // comboBox3
-            // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(180, 137);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(121, 28);
-            this.comboBox3.TabIndex = 2;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(53, 44);
+            this.label1.Location = new System.Drawing.Point(31, 44);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(25, 20);
             this.label1.TabIndex = 3;
@@ -105,16 +81,16 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(42, 93);
+            this.label2.Location = new System.Drawing.Point(31, 93);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.Size = new System.Drawing.Size(39, 20);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Beteg neve";
+            this.label2.Text = "Nev";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(42, 145);
+            this.label3.Location = new System.Drawing.Point(31, 195);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(106, 20);
             this.label3.TabIndex = 5;
@@ -123,43 +99,27 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(42, 212);
+            this.label4.Location = new System.Drawing.Point(31, 250);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(55, 20);
             this.label4.TabIndex = 6;
             this.label4.Text = "Neme";
             // 
-            // comboBox4
+            // nemeComboBox
             // 
-            this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Items.AddRange(new object[] {
+            this.nemeComboBox.FormattingEnabled = true;
+            this.nemeComboBox.Items.AddRange(new object[] {
             "Férfi",
             "Nő"});
-            this.comboBox4.Location = new System.Drawing.Point(180, 195);
-            this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(121, 28);
-            this.comboBox4.TabIndex = 7;
-            // 
-            // comboBox5
-            // 
-            this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(180, 255);
-            this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(121, 28);
-            this.comboBox5.TabIndex = 8;
-            // 
-            // comboBox6
-            // 
-            this.comboBox6.FormattingEnabled = true;
-            this.comboBox6.Location = new System.Drawing.Point(180, 313);
-            this.comboBox6.Name = "comboBox6";
-            this.comboBox6.Size = new System.Drawing.Size(121, 28);
-            this.comboBox6.TabIndex = 9;
+            this.nemeComboBox.Location = new System.Drawing.Point(201, 239);
+            this.nemeComboBox.Name = "nemeComboBox";
+            this.nemeComboBox.Size = new System.Drawing.Size(100, 28);
+            this.nemeComboBox.TabIndex = 7;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(46, 269);
+            this.label5.Location = new System.Drawing.Point(22, 309);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(75, 20);
             this.label5.TabIndex = 10;
@@ -168,7 +128,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(46, 321);
+            this.label6.Location = new System.Drawing.Point(31, 149);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(66, 20);
             this.label6.TabIndex = 11;
@@ -176,13 +136,49 @@
             // 
             // save_btn
             // 
-            this.save_btn.Location = new System.Drawing.Point(357, 66);
+            this.save_btn.Location = new System.Drawing.Point(389, 229);
             this.save_btn.Name = "save_btn";
             this.save_btn.Size = new System.Drawing.Size(81, 47);
             this.save_btn.TabIndex = 12;
             this.save_btn.Text = "Mentés";
             this.save_btn.UseVisualStyleBackColor = true;
             this.save_btn.Click += new System.EventHandler(this.save_btn_Click);
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(201, 44);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(100, 26);
+            this.idTextBox.TabIndex = 13;
+            this.idTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // nevTextBox
+            // 
+            this.nevTextBox.Location = new System.Drawing.Point(201, 93);
+            this.nevTextBox.Name = "nevTextBox";
+            this.nevTextBox.Size = new System.Drawing.Size(100, 26);
+            this.nevTextBox.TabIndex = 14;
+            // 
+            // elerhetosegTextBox
+            // 
+            this.elerhetosegTextBox.Location = new System.Drawing.Point(201, 195);
+            this.elerhetosegTextBox.Name = "elerhetosegTextBox";
+            this.elerhetosegTextBox.Size = new System.Drawing.Size(100, 26);
+            this.elerhetosegTextBox.TabIndex = 15;
+            // 
+            // tajszamTextBox
+            // 
+            this.tajszamTextBox.Location = new System.Drawing.Point(201, 309);
+            this.tajszamTextBox.Name = "tajszamTextBox";
+            this.tajszamTextBox.Size = new System.Drawing.Size(100, 26);
+            this.tajszamTextBox.TabIndex = 16;
+            // 
+            // eletkorTextBox
+            // 
+            this.eletkorTextBox.Location = new System.Drawing.Point(201, 143);
+            this.eletkorTextBox.Name = "eletkorTextBox";
+            this.eletkorTextBox.Size = new System.Drawing.Size(100, 26);
+            this.eletkorTextBox.TabIndex = 17;
             // 
             // Form1
             // 
@@ -203,17 +199,17 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.ComboBox comboBox6;
-        private System.Windows.Forms.ComboBox comboBox5;
-        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox nemeComboBox;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button save_btn;
+        private System.Windows.Forms.TextBox nevTextBox;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.TextBox eletkorTextBox;
+        private System.Windows.Forms.TextBox tajszamTextBox;
+        private System.Windows.Forms.TextBox elerhetosegTextBox;
     }
 }
 
--- Korona/Korona/Form1.cs
+++ Korona/Korona/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,8 +17,20 @@ namespace Korona
         {
             InitializeComponent();
         }
+        SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\tvivu\Desktop\Suli\Corvinus\5.félév\Információs rendszerek fejlesztése\Beadandó projekt\Korona\Korona\Adatbazis.mdf;Integrated Security=True");
 
         private void save_btn_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            string query = "INSERT INTO Betegek (Id,Nev,Eletkor,Elerhetoseg,Neme,Taj_szam) VALUES('"+ idTextBox.Text+ "','" + nevTextBox.Text + "','" + eletkorTextBox.Text + "','" + elerhetosegTextBox.Text + "','" + nemeComboBox.Text + "','" + tajszamTextBox.Text + "')";
+            
+            SqlDataAdapter SDA = new SqlDataAdapter(query,connection);
+            SDA.SelectCommand.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Sikeresen elmentve");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
